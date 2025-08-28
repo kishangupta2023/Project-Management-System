501944c7ea4206050650669848b8db1418466f20
kishan gupta <kishan.2023ug1075@iiitranchi.ac.in>
2025-08-28 06:51:47 +0000

    feat: implement Express server with CORS and MongoDB integration, add healthcheck route, and define API response/error classes

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// basic configurations
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit:"16kb"}));
+app.use(express.static("public"));
+
+// cors configuration
+app.use(
+  cors({
+  origin:process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",credentials:true,
+  methods:["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+  allowedHeaders:["Content-Type","Authorization"],
+}),
+);
+
+// import the routes
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck",healthcheckRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to BaseCampy!")
+})
+
+app.get("/instagram",(req,res)=>{
+    res.send("This is an instagram page");
+})
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import dotenv from "dotenv";
-import express from "express";
+import app from "./app.js";
+import connectDB from "./db/index.js"
+
 dotenv.config({
     path:"./.env",
 });
 
-const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.get("/instagram",(req,res)=>{
-    res.send("This is an instagram page");
+connectDB()
+.then(()=>{
+  app.listen(port,()=>{
+    console.log(`Example app listening on port http://localhost:${port}`);
+  })
 })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+.catch((err)=>{
+  console.error("MongoDB connection error",err)
+  process.exit(1)
 })
