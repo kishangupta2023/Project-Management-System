46163c35a27b89aac8a5297c6165f39042aa2da5
kishan gupta <kishan.2023ug1075@iiitranchi.ac.in>
2025-08-28 16:59:15 +0000

    feat: implement health check controller and async error handler, add user model schema

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,13 +1,23 @@
 import {ApiResponse} from "../utils/api-response.js";
-
-const healthCheck = (req,res) =>{
-    try {
-        res.status(200).json(
-            new ApiResponse(200,{message: "Server is running"})
-        )
-    } catch (error) {
+import { asyncHandler } from "../utils/async-handler.js";
+// const healthCheck =async (req,res) =>{
+//     try {
+//         const user = await getUserFromDB()
+//         res.status(200).json(
+//             new ApiResponse(200,{message: "Server is running"})
+//         )
+//     } catch (error) {
+//         next(err)
         
-    }
-};
+//     }
+// };
+
+const healthCheck = asyncHandler(async (req,res)=>{
+    res
+    .status(200)
+    .json(
+        new ApiResponse(200,{message:"Server is running"})
+    );
+});
 
 export {healthCheck};
