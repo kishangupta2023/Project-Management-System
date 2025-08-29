c3ddcd2be65224a4a44e085b356b12ff5cfaff35
kishan gupta <kishan.2023ug1075@iiitranchi.ac.in>
2025-08-29 11:31:15 +0000

    feat(auth): implement user registration with email verification
    - Added bcrypt for password hashing and jsonwebtoken for token generation.
    - Created auth controller for user registration and token management.
    - Implemented email verification functionality using mailgen and nodemailer.
    - Added validation middleware for user input validation during registration.
    - Defined user registration route and integrated it with the controller and validation.
    - Updated user model to include password hashing and token generation methods.
    - Created utility functions for sending emails and generating email content.

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,80 @@
+import {User} from "../models/user.models.js"
+import {ApiResponse} from "../utils/api-response.js";
+import {ApiError} from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import {emailVerificationMailgenContent, sendEmail} from "../utils/mail.js"
+
+const generateAccessAndRefreshTokens = async (userId)=>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken =user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave:false})
+        return {accessToken,refreshToken}
+    } catch (error) {
+        throw new ApiError(
+            500,
+            "Something went Wrong while generating access token"
+        )
+    }
+}
+
+const registerUser = asyncHandler(async (req,res)=>{
+    const {email,username,password,role}=req.body
+
+    const existedUser =  await User.findOne({
+        $or:[{email},{username}]
+    })
+
+    if(existedUser){
+        throw new ApiError(409,"User Already exists",[])
+    }
+    const user = await User.create({
+        email,
+        password,
+        username,
+        isEmailVerified:false
+    })
+
+    const {unHashedToken,hashedToken,tokenExpiry} =user.generateTemporaryToken();
+
+    user.emailVerificationToken = hashedToken
+    user.emailVerificationExpiry = tokenExpiry
+
+    // i know what i am saving thats why using below code
+    await user.save({validateBeforeSave:false})
+
+    await sendEmail(
+        {
+            email:user?.email,
+            subject:"Please verify your email",
+            mailgenContent:emailVerificationMailgenContent(user.username,
+                `${req.protocol}://${req.get("host")}/api/v1/users/verify-email/${unHashedToken}`
+            ),
+        }
+    );
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken -emailVerificationToken -emailVerifcationExpiry",
+    );
+
+    if(!createdUser){
+        throw new ApiError(500,"Something went wrong while registering a user")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(
+            200,
+            {user:createdUser},
+            "user registered Successfully and verification email has been sent on your email",
+        )
+    )
+});
+
+export {
+    registerUser
+};
